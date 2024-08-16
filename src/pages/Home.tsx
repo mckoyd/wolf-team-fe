e136b98cab59814425ead492dcaf82c2ff4c8766
mckoyd <dominick.mckoy@gmail.com>
2024-08-16 01:25:57 -0400

    adds hero section and bg of highlights section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,49 @@
-import { Grid } from "@mui/material";
+import { Divider, Grid, Typography } from "@mui/material";
 import React from "react";
 import { useHomeStyles } from "../styles/home.styles";
+
+import { ReactComponent as HeroBg } from "../assets/images/bg-pattern-home-2.svg";
+import { ReactComponent as HighlightsBg } from "../assets/images/bg-pattern-home-3.svg";
 
 const Home: React.FC = () => {
   const { classes } = useHomeStyles();
 
   return (
     <Grid container className={classes.homeContainer}>
-      Home page
+      <Grid container direction="column" className={classes.heroContainer}>
+        <Typography variant="h2" className={classes.heroTitle}>
+          Find the best <span className={classes.heroTitleSpan}>talent</span>
+        </Typography>
+        <Typography variant="body2" className={classes.heroBody}>
+          Finding the right people and building high performing teams can be
+          hard. Most companies aren’t tapping into the abundance of global
+          talent. We’re about to change that.
+        </Typography>
+      </Grid>
+      <HeroBg className={classes.heroBg} />
+      <Grid
+        container
+        direction="column"
+        className={classes.highlightsContainer}
+      >
+        <Grid container>
+          <Grid
+            container
+            direction="column"
+            className={classes.highlightsTitleContainer}
+          >
+            <Divider className={classes.highlightsDivider} />
+            <Typography
+              variant="h2"
+              fontSize={"2em"}
+              className={classes.highlightsTitle}
+            >
+              Build & manage distributed teams like no one else.
+            </Typography>
+          </Grid>
+          <HighlightsBg className={classes.highlightsBg} />
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
--- src/styles/home.styles.ts
+++ src/styles/home.styles.ts
@@ -1,5 +1,48 @@
 import { tss } from "tss-react/mui";
 
 export const useHomeStyles = tss.create(({ theme }) => ({
-  homeContainer: {},
+  homeContainer: {
+    background: theme.palette.common.midnightGreen,
+  },
+  heroContainer: {
+    padding: "1em",
+    gap: "1em",
+  },
+  heroTitle: {
+    color: theme.palette.common.white,
+    textAlign: "center",
+    fontSize: "2.5em",
+    padding: "0 1em",
+  },
+  heroTitleSpan: {
+    color: theme.palette.common.lightCoral,
+  },
+  heroBody: {
+    color: theme.palette.common.white,
+    textAlign: "center",
+    padding: "0 1em",
+  },
+  heroBg: {
+    margin: "4em auto 0 auto",
+  },
+  highlightsContainer: {
+    background: theme.palette.common.sacramentoStateGreen,
+  },
+  highlightsTitleContainer: {
+    gap: "2em",
+    width: "65%",
+    padding: "4em 1em",
+  },
+  highlightsDivider: {
+    height: "0.25em",
+    width: "30%",
+    background: theme.palette.common.lightCoral,
+  },
+  highlightsTitle: {
+    color: theme.palette.common.white,
+  },
+  highlightsBg: {
+    width: "35%",
+    paddingLeft: "2em",
+  },
 }));
